6ec823e85e81475352c2dbe57c67d879c3f1fe84
valentinalencioni <143528811+valentinalencioni@users.noreply.github.com>
2024-07-01 03:17:23 -0300

    DemandaService

--- src/services/DemandaService.ts
+++ src/services/DemandaService.ts
@@ -1,5 +1,11 @@
 const BASE_URL ='http://localhost:8082'; 
 
 export const DemandaService ={
-    getDemandas
+    getDemandas: async (): Promise<Demanda[]> => {
+        const response = await fetch(`${BASE_URL}api/v1/demanda`);
+        const data = await response.json();
+        return data;
+    },
+
+    calculateDemanda: async (demanda: DemandaDTO): Promise
 }
