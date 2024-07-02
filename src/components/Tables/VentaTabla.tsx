a504bc6357e783a6ed7fb34847a5947e81edea57
matiFernandezz <151472310+matiFernandezz@users.noreply.github.com>
2024-07-02 00:04:43 -0300

    agrego Filtrado por fecha

--- src/components/Tables/VentaTabla.tsx
+++ src/components/Tables/VentaTabla.tsx
@@ -33,6 +33,8 @@ function VentaTabla() {
     const [showModal, setShowModal] = useState(false);
     const [modalType, setModalType] = useState<ModalType>(ModalType.NONE);
     const [title, setTitle] = useState("");
+    const [fechaInicio, setFechaInicio] = useState<Date | null>(null);
+    const [fechaFin, setFechaFin] = useState<Date | null>(null);
 
     const handleClick = (title: string, venta: Venta, modal: ModalType) => {
         setVenta(venta);
@@ -41,8 +43,63 @@ function VentaTabla() {
         setModalType(modal);
     };
 
+    const handleFechaInicioChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFechaInicio(new Date(e.target.value));
+        console.log("Fecha de inicio:", e.target.value);
+    };
+
+    const handleFechaFinChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFechaFin(new Date(e.target.value));
+        console.log("Fecha de fin:", e.target.value);
+    };
+
+    // Método de filtrado
+    const ventasFiltradas = ventas.filter(venta => {
+        const fechaVenta = new Date(venta.fechaVenta);
+        return (
+            (!fechaInicio || fechaVenta >= fechaInicio) &&
+            (!fechaFin || fechaVenta <= fechaFin)
+        );
+    });
+
     return (
         <>
+            <div className="text-center justify-center">
+                    <h1 className="text-xl font-bold text-center">
+                        Filtrar ventas por fechas
+                    </h1>
+                    <div className="flex text-center justify-center">
+                        <div className="flex flex-col p-4">
+                            <h2 className="font-bold p-2 text-center">
+                                Fecha desde:
+                            </h2>
+                            
+                            <input
+                                type="date"
+                                value={fechaInicio ? fechaInicio.toISOString().substring(0, 10) : ""}
+                                onChange={handleFechaInicioChange}
+                                className="pr-4"
+                        />
+                        </div>
+                        <div className="flex flex-col p-4">
+                            <h2 className="font-bold p-2 text-center">
+                                Fecha hasta:
+                            </h2>
+                            
+                            <input
+                                type="date"
+                                value={fechaFin ? fechaFin.toISOString().substring(0, 10) : ""}
+                                onChange={handleFechaFinChange}
+                                className= "pr-4"
+                                
+                            />
+                        </div>
+                        
+                        
+                    </div>
+                    
+              
+            </div>
             <div className="flex justify-end space-x-2 p-4">
                 <button
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
@@ -52,17 +109,17 @@ function VentaTabla() {
                 </button>
             </div>
             <div>
-                <table className="min-w-full bg-white border border-gray-300">
+                <table className="table table-striped table-hover mt-2 shadow-sm">
                     <thead>
                         <tr>
-                            <th className="py-2 px-4 border-b">Id</th>
-                            <th className="py-2 px-4 border-b">Total Venta</th>
-                            <th className="py-2 px-4 border-b">Fecha de Venta</th>
-                            <th className="py-2 px-4 border-b">Ver Detalle</th>
+                            <th className="py-2 px-4 border-b bg-dark-subtle">Id</th>
+                            <th className="py-2 px-4 border-b bg-dark-subtle">Total Venta</th>
+                            <th className="py-2 px-4 border-b bg-dark-subtle">Fecha de Venta</th>
+                            <th className="py-2 px-4 border-b bg-dark-subtle">Ver Detalle</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        {ventas.map((venta) => (
+                    <tbody className="table-group-divider">
+                        {ventasFiltradas.map((venta) => (
                             <tr key={venta.id}>
                                 <td className="py-2 px-4 border-b">{venta.id}</td>
                                 <td className="py-2 px-4 border-b">{venta.totalVenta}</td>
